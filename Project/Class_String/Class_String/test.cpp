02f1818c7ecd0f8b8eea7845b497f80da637fce0
Raizeroko <1464001349@qq.com>
2023-11-03 19:24:07 +0800

    2023_11_03

--- Project/Class_String/Class_String/test.cpp
+++ Project/Class_String/Class_String/test.cpp
@@ -2,9 +2,94 @@
 
 using namespace zwr;
 
-void test_capacity()
+void test_modifiers()
 {
 	;
+}
+
+void test_iterator()
+{
+	String s1("hello world");
+	auto it = s1.begin();
+	while (it != s1.end())
+	{
+		std::cout << *it;
+		++it;
+	}
+	std::cout << std::endl;
+
+	for (auto e : s1)
+	{
+		std::cout << e << ' ';
+	}
+	std::cout << std::endl;
+
+	for (size_t i = 0; i < s1.size(); i++)
+	{
+		std::cout << s1[i] ;
+	}
+	std::cout << std::endl;
+}
+
+void test_capacity()
+{
+	String s1("hello world");
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	s1.resize(3);
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	s1.resize(10);
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	s1.resize(20);
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	s1.resize(3, 'x');
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	s1.resize(10, 'x');
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	s1.resize(25, 'x');
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	if (s1.empty())
+	{
+		std::cout << "empty" << std::endl;
+	}
+	else
+	{
+		std::cout << "not empty" << std::endl;
+	}
+	s1.clear();
+	std::cout << "**" << s1.c_str() << "**" << std::endl;
+	std::cout << s1.size() << std::endl;
+	std::cout << s1.length() << std::endl;
+
+	if (s1.empty())
+	{
+		std::cout << "empty" << std::endl;
+	}
+	else
+	{
+		std::cout << "not empty" << std::endl;
+	}
+
 }
 
 void test_constructor()
@@ -29,6 +114,7 @@ void test_constructor()
 int main()
 {
 	//test_constructor();
-	test_capacity();
+	//test_capacity();
+	test_iterator();
 	return 0;
 }
