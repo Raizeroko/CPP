61451eeba409a262b30cc581c9455cdc962dd8e6
Raizeroko <1464001349@qq.com>
2023-11-20 22:16:12 +0800

    Coding 2023_11_20

--- Project/Class_Vector/Class_Vector/test.cpp
+++ Project/Class_Vector/Class_Vector/test.cpp
@@ -14,19 +14,28 @@ void test_std_vector()
 	}
 	cout << endl;
 
-	vector<string> v1(v);
+	vector<string>v2(v.begin(), v.end());
 	for (auto e : v)
 	{
 		cout << e << ' ';
 	}
 	cout << endl;
-	cout << v.size()<< ' ' << v.capacity() << endl;
-	v.resize(3);
-	cout << v.size() << ' ' << v.capacity() << endl;
-	v.reserve(3);
-	cout << v.size() << ' ' << v.capacity() << endl;
-	v.reserve(11);
-	cout << v.size() << ' ' << v.capacity() << endl;
+
+
+
+	vector<string> v1(v);
+	for (auto e : v1)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+	cout << v1.size()<< ' ' << v1.capacity() << endl;
+	v1.resize(3);
+	cout << v1.size() << ' ' << v1.capacity() << endl;
+	v1.reserve(3);
+	cout << v1.size() << ' ' << v1.capacity() << endl;
+	v1.reserve(11);
+	cout << v1.size() << ' ' << v1.capacity() << endl;
 
 
 
@@ -34,31 +43,53 @@ void test_std_vector()
 
 void test_constructor()
 {
-	zwr::vector<string> v(10, "asdf");
-	for (int i = 0; i < v.size(); i++)
+	//n个iterator构造
+	zwr::vector<string> vs(10, "asdf");
+	for (int i = 0; i < vs.size(); i++)
 	{
-		cout << v[i] << ' ';
+		cout << vs[i] << ' ';
+	}
+	cout << endl;
+	//iterator构造
+	zwr::vector<string>vs2(vs.begin(), vs.end());
+	for (auto e : vs2)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;	
+	//拷贝构造
+	zwr::vector<string> vs1(vs); 
+	for (auto e : vs1)
+	{
+		cout << e << ' ';
 	}
 	cout << endl;
 
-	zwr::vector<string> v1(v);
+	//深拷贝测试
+	zwr::vector<int> v(10);
+	for (int i = 0; i < 10; i++)
+	{
+		v[i] = i;
+	}
 	for (auto e : v)
 	{
 		cout << e << ' ';
 	}
 	cout << endl;
-	cout << v.size() << ' ' << v.capacity() << endl;
-	v.resize(3);
-	cout << v.size() << ' ' << v.capacity() << endl;
-	v.reserve(3);
-	cout << v.size() << ' ' << v.capacity() << endl;
-	v.reserve(11);
-	cout << v.size() << ' ' << v.capacity() << endl;
+	zwr::vector<zwr::vector<int>> vv(10, v);
+	for (int i = 0; i < vv.size(); i++)
+	{
+		for (int j = 0; j < vv[i].size(); j++)
+		{
+			cout << vv[i][j] << endl;
+		}
+		cout << endl;
+	}
 }
 
 int main()
 {
-	test_std_vector();
+	//test_std_vector();
 	test_constructor();
 	return 0;
 }
