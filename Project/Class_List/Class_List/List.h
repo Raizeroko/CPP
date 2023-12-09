7fb12e30f33f9459bc905db1c1d756f7290ee8f8
Raizeroko <1464001349@qq.com>
2023-12-09 17:59:28 +0800

    Finish 2023_12_09

--- Project/Class_List/Class_List/List.h
+++ Project/Class_List/Class_List/List.h
@@ -2,42 +2,10 @@
 
 #include<iostream>
 #include<algorithm>
+#include<assert.h>
+using namespace::std;
 
 namespace zwr {
-	template<class T>
-	struct list_iterator {
-		typedef list_node<T> node;
-		typedef list_iterator<T> iterator;
-
-		node* _node;
-
-		list_iterator(node* p)
-			:_node(p)
-		{}
-
-		T opeartor* ()
-		{
-			return _node->_val;
-		}
-
-		T& operator++()
-		{
-			_node=_node->_next;
-			return *this;
-		}
-		T& operator++(int)
-		{
-			iterator tmp = _node;
-			_node=_node->next;
-			return tmp;
-		}
-		operator!=(iterator x)
-		{
-			return _node != x._node;
-		}
-
-	};
-
 
 	template<class T>
 	struct list_node {
@@ -51,77 +19,271 @@ namespace zwr {
 		list_node<T>* _prev;
 		list_node<T>* _next;
 	};
+
+	/*template<class T>
+	struct list_const_iterator {
+		typedef list_node<T> node;
+		typedef list_const_iterator<T> self;
+
+		node* _node;
+
+		list_const_iterator(node* p)
+			:_node(p)
+		{}
+
+		const T& operator *()
+		{
+			return _node->_val;
+		}
+		self operator++ ()
+		{
+			_node = _node->_next;
+			return _node;
+		}
+		self operator-- ()
+		{
+			_node = _node->_prev;
+			return _node;
+		}
+		self operator++(int)
+		{
+			self tmp = _node;
+			_node = _node->_next;
+			return tmp;
+		}
+		self operator-- (int)
+		{
+			_node = _node->_prev;
+			return _node;
+		}
+		bool operator!=(self x)
+		{
+			return _node != x._node;
+		}
+		bool operator==(self x)
+		{
+			return _node == x._node;
+		}
+	};*/
+
+
+	template<class T, class Ref, class Ptr>
+	struct list_iterator {
+		typedef list_node<T> node;
+		typedef list_iterator<T, Ref, Ptr> self;
+
+		node* _node;
+
+		list_iterator(node* p)
+			:_node(p)
+		{}
+		Ref operator *()
+		{
+			return _node->_val;
+		}
+		/*const T& operator *() const
+		{
+			return _node->_val;
+		}*/
+
+		/*const iterator operator++ () const
+		{
+			_node = _node->_next;
+			return _node;
+		}*/
+		self operator++ ()
+		{
+			_node=_node->_next;
+			return _node;
+		}
+		self operator-- ()
+		{
+			_node = _node->_prev;
+			return _node;
+		}
+		self operator++(int)
+		{
+			self tmp = _node;
+			_node=_node->_next;
+			return tmp;
+		}
+		self operator-- (int)
+		{
+			_node = _node->_prev;
+			return _node;
+		}
+		bool operator!=(self x)
+		{
+			return _node != x._node;
+		}
+		bool operator==(self x)
+		{
+			return _node == x._node;
+		}
+		/*bool operator!=(iterator x)	const
+		{
+			return _node != x._node;
+		}*/
+		Ptr operator ->()
+		{
+			return &(_node->_val);
+		}
+
+		/*const T* operator->() const
+		{
+			return &(_node->_val);
+		}*/
+	};
+
+
+	
 	
 	template<class T>
 	class list {
-		typedef list_iterator<T> iterator;
-		typedef list_node<T> node;
 	public:
-		
+		typedef list_node<T> node;
+
+		typedef list_iterator<T, T&, T*> iterator;
+		typedef list_iterator<T, const T&, const T*> const_iterator;
+		/*typedef const list_iterator<T> const_iterator;*/
+
 
 		//Constructor
-		list()
+		void empty_init()
 		{
 			_head = new node;
 			_head->_next = _head;
 			_head->_prev = _head;
 		}
-		list(const list& x)
+		list()
 		{
-			//
+			empty_init();
+		}
+		template<class InputIterator>
+		list(InputIterator first, InputIterator last)
+		{
+			empty_init();
+			while (first != last)
+			{
+				push_back(*first);
+				first++;
+			}
+		}
+		list(const list<T>& x)
+		{
+			empty_init();
+			list<T> tmp(x.begin(), x.end());
+			swap(tmp);
+		}
+		list<T>& operator=(list<T> x)
+		{
+			swap(x);
+			return *this;
+		}
+		~list()
+		{
+			while (!empty())
+			{
+				pop_back();
+			}
+			delete _head;
+			_head = nullptr;
 		}
 
 		//Iterator
 		iterator begin()
 		{
-			return iterator(_head->next);
+			return iterator(_head->_next);
 		}
+
+		const_iterator begin() const
+		{
+			return const_iterator(_head->_next);
+		}
+
 		iterator end()
 		{
 			return iterator(_head);
 		}
 
+		const_iterator end() const
+		{
+			return const_iterator(_head);
+		}
+
+
 		//Capacity
 		bool empty()
 		{
-			return _head->_next == _head->_prev;
+			return _head == _head->_prev;
 		}
-		size_t size()
+		size_t size()	
 		{
-			//
+			size_t ret = 0;
+			auto it = begin();
+			while (it++ != end())
+			{
+				ret++;
+			}
+			return ret;
 		}
 
 		//Access
 		T& front()
 		{
-			if (!empty())
-			{
-				return _head->_next->_val;
-			}
+			assert(!empty());
+			return _head->_next->_val;
 		}
+
 		T& back()
 		{
-			if (!empty())
-			{
-				return _head->_prev->_val;
-			}
+			assert(!empty());
+			return _head->_prev->_val;
 		}
 
 
 		//Modifiers
+		iterator insert(iterator pos, const T& val)
+		{
+			node* cur = pos._node;
+			node* prev = cur->_prev;
+			node* new_node = new node(val);
+
+			prev->_next = new_node;
+			new_node->_prev = prev;
+			cur->_prev = new_node;
+			new_node->_next = cur;
+
+			return iterator(new_node);
+		}
+		iterator erase(iterator pos)
+		{
+			assert(!empty());
+			node* cur = pos._node;
+			node* prev = cur->_prev;
+			node* next = cur->_next;
+
+			next->_prev = prev;
+			prev->_next = next;
+			delete cur;
+			return iterator(next);
+		}
+		
 		void push_back(const T& val)
 		{
-			node* tmp = new node(val);
+			/*node* tmp = new node(val);
 			node* tail = _head->_prev;
 
 			_head->_prev = tmp;
 			tmp->_next = _head;
 			tail->_next = tmp;
-			tmp->_prev = tail;
+			tmp->_prev = tail;*/
+			insert(end(), val);
+
 		}
 		void pop_back()
 		{
-			if (empty)
+			/*if (empty)
 				return;
 
 			node* tail = _head->_prev;
@@ -129,21 +291,23 @@ namespace zwr {
 			new_tail->_next = _head;
 			_head->_prev = new_tail;
 
-			delete tail;
+			delete tail;*/
+			erase(--end());
 		}
 		void push_front(const T& val)
 		{
-			node* tmp = new node(val);
+			/*node* tmp = new node(val);
 			node* front = _head->_next;
 
 			_head->_next = tmp;
 			tmp->_prev = _head;
 			front->prev = tmp;
-			tmp->_next = front;
+			tmp->_next = front;*/
+			insert(begin(), val);
 		}
 		void pop_front()
 		{
-			if (empty)
+			/*if (empty)
 				return;
 
 			node* front = _head->_next;
@@ -151,15 +315,20 @@ namespace zwr {
 			_head->_next = new_front;
 			new_front->_prev = _head;
 
-			delete front;
+			delete front;*/
+			erase(begin());
 		}
 		void swap(list<T>& x)
 		{
-			swap(_head, x);
+			std::swap(_head, x._head);
 		}
-
-	
-		
+		void clear()
+		{
+			while (!empty())
+			{
+				pop_back();
+			}
+		}
 
 	private:
 		node* _head;
--- Project/Class_List/Class_List/test.cpp
+++ Project/Class_List/Class_List/test.cpp
@@ -1,21 +1,60 @@
 #include "List.h"
+template<class T>
+void print(const zwr::list<T>& l)
+{
+	auto it = l.begin();
+	//zwr::list<int>::const_iterator it = l.begin();
+	//it = l.end();
+	//cout << *it;
+	while (it != l.end())
+	{
+		cout << it->_year << ' ';
+		cout << it->_month <<' ';
+		cout << it->_day <<' ';
+		cout << endl;
+		++it;
+	}
+	/*for (auto& e : l)
+	{
+		e++;
+		cout << e << ' ';
+
+	}*/
+	cout << endl;
+}
+
+struct Date {
+	Date()
+		: _year(2023)
+		, _month(12)
+		, _day(9)
+	{}
+	int _year;
+	int _month;
+	int _day;
+};
+
+void test_const_iterator()
+{
+	zwr::list<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+	list.push_back(5);
+	//print(list);
+
+	zwr::list<Date> list_date;
+	list_date.push_back(Date());
+	list_date.push_back(Date());
+	list_date.push_back(Date());
+	list_date.push_back(Date());
+	print(list_date);
+
+}
+
 
 void test_iterator()
-{
-
-}
-
-void test_access()
-{
-
-}
-
-void test_capacity()
-{
-
-}
-
-void test_modifiers()
 {
 	zwr::list<int> list;
 	list.push_back(1);
@@ -24,15 +63,167 @@ void test_modifiers()
 	list.push_back(4);
 	list.push_back(5);
 
+	zwr::list<int>::iterator it = list.begin();
+	while (it != list.end())
+	{
+		cout << *it << ' ';
+		++it;
+	}
+	cout << endl;
+	for (auto& e : list)
+	{
+		cout << ++e << ' ';
+	}
+	cout << endl;
+
+	zwr::list<Date> list_date;
+	list_date.push_back(Date());
+	list_date.push_back(Date());
+	list_date.push_back(Date());
+	list_date.push_back(Date());
+
+	auto dit = list_date.begin();
+	cout << ++dit->_year;
+	cout << ++dit->_month;
+	cout << ++dit->_day;
+
+	return;
+}
+
+void test_access()
+{
+	zwr::list<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+	list.push_back(5);
+
+	cout << list.front()<< ' ';
+	cout << list.back();
+}
+
+void test_capacity()
+{
+	zwr::list<int> list;
+	if (list.empty())
+	{
+		list.push_back(1);
+		list.push_back(2);
+		list.push_back(3);
+		list.push_back(4);
+		list.push_back(5);
+	}
+	cout << list.size();
+}
+
+void test_modifiers()
+{
+	zwr::list<int> list;
+	list.push_back(5);
+	list.push_back(4);
+	list.push_back(3);
+	list.push_back(2);
+	list.push_back(1);
+	
+	zwr::list<int> tmp;
+	tmp.push_back(1);
+	tmp.push_back(2);
+	tmp.push_back(3);
+	tmp.push_back(4);
+	tmp.push_back(5);
+
+	list.swap(tmp);
+	for (auto e : list)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+	for (auto e : tmp)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+
+	list.clear();
+	if (list.empty())
+	{
+		cout << "empty" << endl;
+	}
+
+	list.push_front(5);
+	list.push_front(4);
+	list.push_front(3);
+	list.push_front(2);
+	list.push_front(1);
+	for (auto& e : list)
+	{
+		cout << e << ' ';
+
+	}
+	cout << endl;
+	list.pop_front();
+	list.pop_front();
+	list.pop_front();
+	list.pop_front();
+	list.pop_front();
+	//list.pop_front();
+	if (list.empty())
+	{
+		cout << "empty" << endl;
+	}
+
+
+
+}
+void test_constructor()
+{
+	zwr::list<int> list1;
+	list1.push_back(1);
+	list1.push_back(2);
+	list1.push_back(3);
+	list1.push_back(4);
+	list1.push_back(5);
+	for (auto e : list1)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+
+	zwr::list<int> list2(list1);
+	for (auto e : list2)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+
+	zwr::list<int> list3;
+	list3 = list1;
+	for (auto e : list3)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+
+	zwr::list<int> list4(list1.begin(), list1.end());
+	for (auto e : list4)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+
+
 
 }
 
-
 int main()
 {
-
-
-	test_modifiers();
+	//test_const_iterator();
+	//test_iterator();
+	//test_access();
+	//test_capacity();
+	//test_modifiers();
+	test_constructor();
 
 
 	return 0;
