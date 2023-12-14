76c5a440cb8f8624cc8f252339eb50bc33b5ba13
Raizeroko <1464001349@qq.com>
2023-12-14 22:05:40 +0800

    stack_list 2023_12_14

--- Practice/CppAlgotithmQuestions/CppAlgotithmQuestions/AQ_vector.h
+++ Practice/CppAlgotithmQuestions/CppAlgotithmQuestions/AQ_vector.h
@@ -37,7 +37,7 @@ public:
         return ret;
     }
     //------------------------------------------------------------------//
-    vector<int> singleNumber(vector<int>& nums) 
+    vector<int> singleNumber2to2(vector<int>& nums) 
     {
         int sum = 0;
         for (auto e : nums)
@@ -77,4 +77,136 @@ public:
         ret.push_back(sum2);
         return ret;
     }
+    //------------------------------------------------------------------//
+    int singleNumber3to1(vector<int>& nums) 
+    {
+        int ret = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            int sum = 0;
+            for (auto e : nums)
+            {
+                sum += ((e >> i) & 1);
+            }
+            if (sum % 3)
+            {
+                ret |= (1 << i);
+            }
+        }
+        return ret;
+    }
+    //------------------------------------------------------------------//
+    vector<vector<int>> generate(int numRows) 
+    {
+        vector<vector<int>> ret;
+        for (int i = 1; i <= numRows; i++)
+        {
+            vector<int> v(i, 1);
+            ret.push_back(v);
+        }
+        for (int i = 2; i < ret.size(); i++)
+        {
+            //除去每行第一个和最后一个元素
+            for (int j = 1; j < ret[i].size() - 1; j++)
+            {
+                ret[i][j] = ret[i - 1][j - 1] + ret[i - 1][j];
+            }
+        }
+        return ret;
+    }
+    //------------------------------------------------------------------//
+    int removeDuplicates(vector<int>& nums) 
+    {
+        int first = 0;
+        int last = 0;
+        while (last < nums.size())
+        {
+            if (nums[first] == nums[last])
+            {
+                last++;
+            }
+            else
+            {
+                first++;
+                nums[first] = nums[last];
+            }
+        }
+        return first + 1;
+    }
+    //------------------------------------------------------------------//
+    void test()
+    {
+        int ar[] = { 1,2,3,4,0,5,6,7,8,9 };
+        int n = sizeof(ar) / sizeof(int);
+        vector<int> v(ar, ar + n);
+        vector<int>::iterator it = v.begin();
+        while (it != v.end())
+
+        {
+
+            if (*it != 0)
+
+                cout << *it;
+
+            else
+
+                v.erase(it);
+
+            it++;
+
+        }
+    }
+    //------------------------------------------------------------------//
+    int FindGreatestSumOfSubArray(vector<int>& array) 
+    {
+        // write code here
+        vector<int> ret(array.size());
+        ret[ret.size() - 1] = array[array.size() - 1];
+        for (int i = array.size() - 2; i >= 0; i--)
+        {
+            if (array[i] > array[i] + ret[i + 1])
+            {
+                ret[i] = array[i];
+            }
+            else
+            {
+                ret[i] = array[i] + ret[i + 1];
+            }
+        }
+        int max = ret[0];
+        for (int tmp : ret)
+        {
+            max = max > tmp ? max : tmp;
+        }
+        return max;
+    }
+    //------------------------------------------------------------------//
+    int MoreThanHalfNum_Solution(vector<int>& numbers) {
+        // write code here
+        int ret = -1;
+        int count = 0;
+        for (auto num : numbers)
+        {
+            if (ret == num || count == 0)
+            {
+                ret = num;
+                count++;
+            }
+            if (ret != num)
+            {
+                count--;
+            }
+        }
+        return ret;
+    }
+    //------------------------------------------------------------------//
+    int singleNumber2to1(vector<int>& nums) {
+        int ret = 0;
+        for (int num : nums)
+        {
+            ret ^= num;
+        }
+        return ret;
+    }
+    //------------------------------------------------------------------//
 };
--- Practice/CppAlgotithmQuestions/CppAlgotithmQuestions/main.cpp
+++ Practice/CppAlgotithmQuestions/CppAlgotithmQuestions/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include"AQ_string.h"
 #include"AQ_vector.h"
+#include"AQ_stack_queue.h"
 
 void test_AQ_string()
 {
@@ -20,14 +21,29 @@ void test_AQ_vector()
 	}
 	cout << endl;
 	vector<int> vec = { 1,2,1,3,2,5 };
-	aq.singleNumber(vec);
+	aq.singleNumber2to2(vec);
+	//aq.test();
+	vector<int> v = { 1,2,3,2,2,2,5,4,2 };
+	aq.MoreThanHalfNum_Solution(v);
 
 }
+
+void test_AQ_stack_queue()
+{
+	AQ_stack_queue aq;
+	vector<string> input={ "10","6","9","3","+","-11","*","/","*","17","+","5","+" };
+	cout<<aq.evalRPN(input);
+	vector<int> pushV = { 2,1,0 };
+	vector<int> popV = { 1,2,0 };
+
+	aq.IsPopOrder(pushV, popV);
+}
+
 
 int main()
 {
 	//test_AQ_string();
-	test_AQ_vector();
-
+	//test_AQ_vector();
+	test_AQ_stack_queue();
 	
 }
