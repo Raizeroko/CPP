301b55adcebd9983a481746184b349d7b5e685c4
Raizeroko <1464001349@qq.com>
2024-11-27 22:44:49 +0800

    Leetcode_Stack&Queue_2024_11_27

---
+++ Practice/Leetcode_Stack&Queue/Leetcode_Stack&Queue/IsSubsequence.cpp
@@ -0,0 +1,24 @@
+#include <queue>
+#include <string>
+
+using namespace std;
+
+// MySolution
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        queue<char> q;
+        for (auto e : s) {
+            q.push(e);
+        }
+        for (auto e : t) {
+            if (e == q.front()) {
+                q.pop();
+            }
+        }
+        if (q.empty()) {
+            return true;
+        }
+        return false;
+    }
+};
