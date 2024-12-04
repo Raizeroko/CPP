f72de91cb218fb5e46856386f81bcdd3116de51f
Raizeroko <1464001349@qq.com>
2024-12-04 21:06:46 +0800

    Leetcode_Vector_2024_12_04

---
+++ Practice/Leetcode_Vector/Leetcode_Vector/HScore.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// MySolution
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int h = citations.size();
+        while (h)
+        {
+            int tmp;
+            auto it = citations.begin();
+            for (tmp = 0; it != citations.end(); it++)
+            {
+                if (*it >= h)
+                {
+                    tmp++;
+                }
+            }
+            if (tmp >= h) {
+                break;
+            }
+            h--;
+        }
+        return h;
+    }
+};
+
+// OptimSolution
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int h = 0;
+        for (int i = citations.size() - 1; i >= 0; i--) {
+            if (citations[i] > h) {
+                h++;
+            }
+            else {
+                break;
+            }
+        }
+        return h;
+    }
+};
