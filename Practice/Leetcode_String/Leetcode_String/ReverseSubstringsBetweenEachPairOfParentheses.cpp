0485f92ef056f93c956a86005c54b1614f14accc
Raizeroko <1464001349@qq.com>
2024-12-18 19:32:59 +0800

    Leetcode_String_2024_12_18

---
+++ Practice/Leetcode_String/Leetcode_String/ReverseSubstringsBetweenEachPairOfParentheses.cpp
@@ -0,0 +1,25 @@
+#include <string>
+
+using namespace std;
+
+// MySolution
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        string ret;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ')') {
+                string tmp;
+                while (ret.back() != '(') {
+                    tmp += ret.back();
+                    ret.pop_back();
+                }
+                ret.pop_back();
+                ret += tmp;
+                continue;
+            }
+            ret += s[i];
+        }
+        return ret;
+    }
+};
