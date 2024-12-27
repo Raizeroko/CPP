2308e1ef2b8dde05eecb18ccdc7d1fbe5438f1fc
Raizeroko <1464001349@qq.com>
2024-12-27 21:16:14 +0800

    Leetcode_String_2024_12_27

---
+++ Practice/Leetcode_String/Leetcode_String/LongestPalindrome.cpp
@@ -0,0 +1,20 @@
+#include "string.h"
+
+// MySolution
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> harsh;
+        for (auto c : s) {
+            harsh[c]++;
+        }
+        int ret = 0;
+        for (auto pair : harsh) {
+            int value = pair.second;
+            ret += value / 2 * 2;
+            if (value % 2 == 1 and ret % 2 == 0)
+                ret++;
+        }
+        return ret;
+    }
+};
