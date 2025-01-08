25e9be444a860eeeb1aa1dfa85f43a839f1658cf
Raizeroko <1464001349@qq.com>
2025-01-08 21:58:17 +0800

    Leetcode_Harsh_2025_01_08

--- Practice/Leetcode_Harsh/Leetcode_Harsh/SortCharactersByFrequency.cpp
+++ Practice/Leetcode_Harsh/Leetcode_Harsh/SortCharactersByFrequency.cpp
@@ -5,6 +5,33 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char, int> mp;
+        int length = s.length();
+        for (auto& ch : s) {
+            mp[ch]++;
+        }
+        vector<pair<char, int>> vec;
+        for (auto& it : mp) {
+            vec.emplace_back(it);
+        }
+        sort(vec.begin(), vec.end(), [](const pair<char, int>& a, const pair<char, int>& b) {
+            return a.second > b.second;
+            });
+        string ret;
+        for (auto& [ch, num] : vec) {
+            for (int i = 0; i < num; i++) {
+                ret.push_back(ch);
+            }
+        }
+        return ret;
+    }
+};
+
+
+// OptimSolution
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
         for (auto ch : s) mp[ch]++;
         sort(s.begin(), s.end(), [&](const char& a, const char& b) {
             return mp[a] == mp[b] ? a > b : mp[a] > mp[b];
@@ -12,4 +39,19 @@ public:
         return s;
     }
 };
+
+
+// Review (2025_01_08)
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> harsh;
+        for (auto c : s) harsh[c]++;
+        sort(s.begin(), s.end(), [&](char& a, char& b) {
+            return harsh[a] == harsh[b] ? a > b : harsh[a] > harsh[b];
+            });
+        return s;
+    }
+};
+
 
