4ff1d8d73ea1fcde8cd0b9286c1e0ecb4f48a778
Raizeroko <1464001349@qq.com>
2025-02-12 22:45:48 +0800

    Leetcode_Puzzle_2025_02_12

---
+++ Practice/Leetcode_Puzzle/Leetcode_Puzzle/TheMaximumOddDivisor.cpp
@@ -0,0 +1,28 @@
+#include "Puzzle.h"
+
+// Nowcoder
+// MySolution
+#include <iostream>
+using namespace std;
+
+long long int GetDivisor(long long int num) {
+    long long int ret = 0;
+    for (int i = 1; i <= num; i += 2) {
+        ret += i;
+    }
+    return ret;
+}
+
+int main() {
+    int a, b;
+    while (cin >> a) { // 注意 while 处理多个 case
+        long long int ret = 0;
+        while (a) {
+            // cout << GetDivisor(i) << endl;
+            ret += GetDivisor(a);
+            a /= 2;
+        }
+        cout << ret << endl;
+
+    }
+}
