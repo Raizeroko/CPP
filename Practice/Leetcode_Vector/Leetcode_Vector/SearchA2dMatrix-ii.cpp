95b83c8c134a19f75c93e9ad20109b6cdb170e23
Raizeroko <1464001349@qq.com>
2025-03-18 22:45:32 +0800

    Leetcode_Vector_2025_03_18

--- Practice/Leetcode_Vector/Leetcode_Vector/SearchA2dMatrix-ii.cpp
+++ Practice/Leetcode_Vector/Leetcode_Vector/SearchA2dMatrix-ii.cpp
@@ -21,3 +21,25 @@ public:
         return false;
     }
 };
+
+
+// Review 2025/03/18
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i = matrix.size() - 1;
+        int j = 0;
+        while (i >= 0 && j <= matrix[0].size() - 1) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] > target) {
+                i--;
+            }
+            else {
+                j++;
+            }
+        }
+        return false;
+    }
+};s
