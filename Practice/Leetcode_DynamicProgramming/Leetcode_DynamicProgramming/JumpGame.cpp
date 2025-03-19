d0c24c5f487dc0b6a1865b39c5f36e3b34072ed9
Raizeroko <1464001349@qq.com>
2025-03-19 22:09:43 +0800

    Leetcode_DynamicProgramming_2025_03_19

--- Practice/Leetcode_DynamicProgramming/Leetcode_DynamicProgramming/JumpGame.cpp
+++ Practice/Leetcode_DynamicProgramming/Leetcode_DynamicProgramming/JumpGame.cpp
@@ -17,3 +17,20 @@ public:
         return false;
     }
 };
+
+// Review 2025/03/18
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int jump = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i <= jump) {
+                jump = max(jump, nums[i] + i);
+                if (jump >= nums.size() - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
