fbd478a1129a2d2eec5450fa83b79eb39e96123b
Raizeroko <1464001349@qq.com>
2025-03-25 20:44:46 +0800

    Leetcode_Puzzle 2025_03_25

---
+++ Practice/Leetcode_Puzzle/Leetcode_Puzzle/SingleNumber.cpp
@@ -0,0 +1,13 @@
+#include "Puzzle.h"
+
+// MySolution
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ret ^= nums[i];
+        }
+        return ret;
+    }
+};
