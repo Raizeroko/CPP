aa0e8c48ac68f7aa164a036a9e1a575bb56cb094
Raizeroko <1464001349@qq.com>
2025-03-26 21:08:43 +0800

    Leetcode_String 2025_03_26

--- Practice/Leetcode_String/Leetcode_String/MinimumWindowSubstring.cpp
+++ Practice/Leetcode_String/Leetcode_String/MinimumWindowSubstring.cpp
@@ -27,3 +27,37 @@ public:
         return ret;
     }
 };
+
+
+// Review 2025/03/26
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> harsh;
+        for (auto ch : t) {
+            harsh[ch]++;
+        }
+        int len = s.size();
+        int diff = harsh.size();
+        int min_len = len + 1;
+        int ans_l = -1;
+        int l = 0, r = 0;
+        while (r < len) {
+            if (harsh.count(s[r]) && --harsh[s[r]] == 0) {
+                diff--;
+            }
+            while (diff == 0 && l <= r) {
+                if (min_len > r - l + 1) {
+                    min_len = r - l + 1;
+                    ans_l = l;
+                }
+                if (harsh.count(s[l]) && ++harsh[s[l]] > 0) {
+                    diff++;
+                }
+                l++;
+            }
+            r++;
+        }
+        return min_len < len + 1 ? s.substr(ans_l, min_len) : "";
+    }
+};
