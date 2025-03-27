09edb8dd88120ed7a0f3fd126b14c9488b10006e
Raizeroko <1464001349@qq.com>
2025-03-27 21:45:18 +0800

    Make compile_run into net service 2025_03_27

--- Project/Project_OnlineJudge/common/utils.hpp
+++ Project/Project_OnlineJudge/common/utils.hpp
@@ -6,6 +6,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <atomic>
+#include <fstream>
+
+
 namespace com_utils
 {
     const std::string temp_path = "./temp/";
@@ -15,11 +19,17 @@ namespace com_utils
     public:
         static std::string GetTimeStamp()
         {
+            struct timeval time_stamp;
+            gettimeofday(&time_stamp, nullptr);
+            return std::to_string(time_stamp.tv_sec);
+        }
+        //获得毫秒时间戳
+        static std::string GetTimeMs()
+        {
             struct timeval _time;
             gettimeofday(&_time, nullptr);
-            return std::to_string(_time.tv_sec);
+            return std::to_string(_time.tv_sec * 1000 + _time.tv_usec / 1000);
         }
-
     };
 
     class PathUtil
@@ -74,6 +84,38 @@ namespace com_utils
             {
                 return false;
             }
+            return true;
+        }
+        static std::string UniqueFileName()
+        {
+            static std::atomic_uint id(0);
+            id++;
+            // 毫秒级时间戳+原子性递增唯一值: 来保证唯一性
+            std::string ms = TimeUtil::GetTimeMs();
+            std::string uniq_id = std::to_string(id);
+            return ms + "_" + uniq_id;
+        }
+
+        static bool WriteFile(const std::string& path_name, const std::string& content){
+            std::ofstream write_file(path_name);
+            if(!write_file){
+                return false;
+            }
+            write_file << content;
+            write_file.close();  // 确保关闭文件
+            return true;
+        }
+        static bool ReadFile(const std::string& path_name, std::string& content){
+            content.clear();
+            std::ifstream read_file(path_name);
+            if(!read_file){
+                return false;
+            }
+            std::string line;
+            while(getline(read_file, line)){
+                content += line;
+            }
+            read_file.close();
             return true;
         }
     };
