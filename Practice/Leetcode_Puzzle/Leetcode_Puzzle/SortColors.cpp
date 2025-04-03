09e3a9cdb34a433475a2398cd4067f109bda8712
Raizeroko <1464001349@qq.com>
2025-04-03 19:23:01 +0800

    Leetcode_Puzzle 2025_04_03

---
+++ Practice/Leetcode_Puzzle/Leetcode_Puzzle/SortColors.cpp
@@ -0,0 +1,26 @@
+#include "Puzzle.h"
+
+// MySolution
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int cur = 0;
+        while (cur <= right) {
+            if (nums[cur] == 0) {
+                swap(nums[left], nums[cur]);
+                left++;
+                cur++;
+            }
+            else if (nums[cur] == 2) {
+                swap(nums[right], nums[cur]);
+                right--;
+            }
+            else {
+                cur++;
+            }
+        }
+        return;
+    }
+};
