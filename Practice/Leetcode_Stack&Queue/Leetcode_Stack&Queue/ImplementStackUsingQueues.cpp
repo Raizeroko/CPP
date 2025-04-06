7809a7a56ec8417f87fcdb2c21f398cdc111c583
Raizeroko <1464001349@qq.com>
2025-04-06 22:03:43 +0800

    Leetcode_Stack&Queue 2025_04_06

--- Practice/Leetcode_Stack&Queue/Leetcode_Stack&Queue/ImplementStackUsingQueues.cpp
+++ Practice/Leetcode_Stack&Queue/Leetcode_Stack&Queue/ImplementStackUsingQueues.cpp
@@ -37,3 +37,41 @@ private:
     queue<int>* qPush;
     queue<int>* qPop;
 };
+
+
+// Review 2025/04/06
+class MyStack {
+    queue<int>* _qPush;
+    queue<int>* _qPop;;
+public:
+    MyStack() {
+        _qPush = new queue<int>;
+        _qPop = new queue<int>;
+    }
+
+    void push(int x) {
+        _qPush->push(x);
+        while (!_qPop->empty()) {
+            _qPush->push(_qPop->front());
+            _qPop->pop();
+        }
+        swap(_qPush, _qPop);
+    }
+
+    int pop() {
+        int tmp = -1;
+        if (!_qPop->empty()) {
+            tmp = _qPop->front();
+            _qPop->pop();
+        }
+        return tmp;
+    }
+
+    int top() {
+        return _qPop->front();
+    }
+
+    bool empty() {
+        return _qPop->empty();
+    }
+};
