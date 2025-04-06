fbd5ba942ce82af7c216524f11ac4da8c577d376
Raizeroko <1464001349@qq.com>
2025-04-06 22:04:19 +0800

    Leetcode_Tree 2025_04_06

--- Practice/Leetcode_Tree/Leetcode_Tree/PathSum_III.cpp
+++ Practice/Leetcode_Tree/Leetcode_Tree/PathSum_III.cpp
@@ -29,3 +29,46 @@ public:
     }
 };
 
+// Review 2025/04/06
+class Solution {
+    int ret = 0;
+public:
+    void hasPathSum(TreeNode* root, long targetSum) {
+        if (!root) return;
+        targetSum -= root->val;
+        if (targetSum == 0) {
+            ret++;
+        }
+        hasPathSum(root->left, targetSum);
+        hasPathSum(root->right, targetSum);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+        hasPathSum(root, targetSum);
+        pathSum(root->left, targetSum);
+        pathSum(root->right, targetSum);
+        return ret;
+    }
+};
+
+// OptimSolution
+class Solution {
+    unordered_map<long, long> prefix;
+    int ret = 0;
+public:
+    void dfs(TreeNode* root, int targetSum, long sum) {
+        if (!root) return;
+        sum += root->val;
+        if (prefix.count(sum - targetSum)) ret += prefix[sum - targetSum];
+        prefix[sum]++;
+        dfs(root->left, targetSum, sum);
+        dfs(root->right, targetSum, sum);
+        prefix[sum]--;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        prefix[0] = 1;
+        dfs(root, targetSum, 0);
+        return ret;
+    }
+};
