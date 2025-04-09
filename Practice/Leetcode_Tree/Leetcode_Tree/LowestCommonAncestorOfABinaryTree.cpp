57dc4e08465f7593f0860c2d7697aa720b901a5a
Raizeroko <1464001349@qq.com>
2025-04-09 22:05:09 +0800

    Leetcode_Tree 2025_04_09

--- Practice/Leetcode_Tree/Leetcode_Tree/LowestCommonAncestorOfABinaryTree.cpp
+++ Practice/Leetcode_Tree/Leetcode_Tree/LowestCommonAncestorOfABinaryTree.cpp
@@ -27,3 +27,23 @@ public:
         return left;
     }
 };
+
+// Review 2025/04/09
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return nullptr;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        // 当前节点是其中一个节点
+        // 1.该节点第一个被找到，继续往上找，返回当前节点
+        // 2.该节点是第二个被找到，当前节点就是结果，返回当前节点
+        if (root == p || root == q) return root;
+        // 左右分支各找到一个节点，那该节点就是公共祖先，返回该节点
+        if (left && right) return root;
+        // 左右分支有其中一个找到
+        if (left) return left;
+        if (right) return right;
+        return nullptr;
+    }
+};
