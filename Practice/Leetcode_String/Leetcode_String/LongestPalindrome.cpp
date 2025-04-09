b6dfa6807239a9d6173043311228ee2e09314fcc
Raizeroko <1464001349@qq.com>
2025-04-09 22:03:37 +0800

    Leetcode_String 2025_04_09

--- Practice/Leetcode_String/Leetcode_String/LongestPalindrome.cpp
+++ Practice/Leetcode_String/Leetcode_String/LongestPalindrome.cpp
@@ -18,3 +18,26 @@ public:
         return ret;
     }
 };
+
+// Review 2025/04/09
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> harsh;
+        for (auto ch : s) {
+            harsh[ch]++;
+        }
+        int max_odd = 0;
+        int ret = 0;
+        int flag = false;
+        for (auto kv : harsh) {
+            if (kv.second % 2 == 0) ret += kv.second;
+            else {
+                ret += kv.second - 1;
+                flag = true;
+            }
+        }
+        if (flag) ret += 1;
+        return ret;
+    }
+};
