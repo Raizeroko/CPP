ed9a7cc9494ba2a265753b131953be2673311a13
Raizeroko <1464001349@qq.com>
2025-04-09 22:02:55 +0800

    Leetcode_List 2025_04_09

--- Practice/Leetcode_List/Leetcode_List/RemoveDuplicatesFromSortedList.cpp
+++ Practice/Leetcode_List/Leetcode_List/RemoveDuplicatesFromSortedList.cpp
@@ -25,3 +25,26 @@ public:
         return head;
     }
 };
+
+
+// Review 2025/04/09
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return head;
+        ListNode* cur = head->next;
+        ListNode* prev = head;
+        while (cur) {
+            if (cur->val == prev->val) {
+                prev->next = cur->next;
+                delete cur;
+                cur = prev->next;
+            }
+            else {
+                prev = prev->next;
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
