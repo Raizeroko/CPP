b53c26893554409f5c868e65377673871848b85b
Raizeroko <1464001349@qq.com>
2025-09-21 21:33:34 +0800

    2025_09_21

--- Practice/Leetcode_Puzzle/Leetcode_Puzzle/FindFirstAndLastPositionOfElementInSortedArray.cpp
+++ Practice/Leetcode_Puzzle/Leetcode_Puzzle/FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -32,3 +32,32 @@ public:
         return ret;
     }
 };
+
+
+// Review 2025/09/21
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return { -1, -1 };
+        int l = 0;
+        int r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        int begin = l;
+        if (begin >= n || nums[begin] != target) return { -1, -1 };
+        l = 0;
+        r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+        int end = r;
+        return { begin, end };
+
+    }
+};
